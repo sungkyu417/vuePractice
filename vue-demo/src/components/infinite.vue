424437104cc9a584d7044338884bc82ccac5c5b0
Administrator <Administrator@Sonal>
2016-11-20 15:50:08 +0800

    new

--- vue-demo/src/components/infinite.vue
+++ vue-demo/src/components/infinite.vue
@@ -1,6 +1,20 @@
 <template>
 	<div>
 	<p>Infinite</p>
-	<p>sungkyu</p>
+	<ul v-for="name in members.name">
+		<li>{{name}}</li>
+	</ul>
 	</div>
 </template>
+<script>
+	export default{
+		data(){
+			let members={
+				name:['sungkyu','hoya','sungyeol']
+			}
+			return{
+				members:members
+			}
+		}
+	}
+</script>
